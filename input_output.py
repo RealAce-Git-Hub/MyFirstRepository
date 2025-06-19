55083e5f339719748ec2dcc8d8ea45ee9a9dc319
RealAce-Git-Hub <tyler.tuazon@gmail.com>
2025-06-19 21:40:05 +0700

    Input Output Practice

--- input_output.py
+++ input_output.py
@@ -1,2 +1,16 @@
-x = input("Enter a name: ")
-print("Hello", x, "Welcome to the world of Python")
+# calculating the area of a square
+a = int(input("Enter the side of the square: "))
+area_of_square = a * a
+perimeter = 4 * a
+
+print("The area of the square is: ",area_of_square)
+print("The perimeter of the square is: ",perimeter)
+print("-"*70)
+#calculating the area of a rectangle
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+area_of_rectangle = length * width
+perimeter_of_rectangle =  2*(length + width)
+
+print("The area of the rectangle is: ", area_of_rectangle)
+print("The perimeter of the rectangle is: ", perimeter_of_rectangle)
