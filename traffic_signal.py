49e8a1f1486b781f25d130600967ea2092e5c8c4
RealAce-Git-Hub <tyler.tuazon@gmail.com>
2025-06-19 22:09:23 +0700

    Updated traffic signal

---
+++ traffic_signal.py
@@ -0,0 +1,8 @@
+signal = input("Enter the color of the traffic signal: ")
+
+if signal == "Green":
+    print("Start driving!")
+elif (signal == "Yellow") or (signal == "yellow"):
+    print("Start the engine of your car!")
+else:
+    print("Don't start the car!")
