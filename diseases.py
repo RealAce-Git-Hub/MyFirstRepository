eac652297e4915cef67919f773c576c4e5993f90
RealAce-Git-Hub <tyler.tuazon@gmail.com>
2025-06-26 22:31:34 +0700

    updated and_diseases.py

--- diseases.py
+++ diseases.py
@@ -1,20 +1,29 @@
-disease1 = "malaria"
-disease1 = "flu"
+disease1 = "Malaria"
+disease2 = "Flu"
 disease3 = "Cold"
 disease4 = "Migrane"
 
-tempurature = float(input("What is your tempurature in celsius?: "))
-symptom1 = input("Enter symptoms 1: ")
-symptom2 = input("Enter symptom 2: ")
+temperature = float(input("What is your temperature in celsius?: "))
+symptom1 = input("Enter your first symptom: ")
+symptom2 = input("Enter your second symptom: ")
 
-if (tempurature >= 36.1) and (tempurature <= 37.2):
-    print("Your tempurature is normal, you are not sick.")
+if (temperature >= 36.1) and (temperature <= 37.2):
+    print("Your temperature is normal, you may not be sick.")
 
-elif (tempurature > 37.2):
-    print("Your tempurate is above normal, you might be sick.")
+    if (symptom1 == "cough") and (symptom2 == "stuffy nose"):
+        print("Your symptoms show that you might have a Cold.")
+
+    elif (symptom1 == "headache") and (symptom2 == "dizziness"):
+        print("Your symptoms show you might have a Migrane.")
+
+elif (temperature > 37.2):
+    print("Your temperature is above normal, you might be sick.")
 
     if (symptom1 == "headache") and (symptom2 == "nausea"):
         print("Your symptoms show that you might have Malaria.")
-    
+
     elif (symptom1 == "cough") and (symptom2 == "sore throat"):
-        print("Your symptoms show that you might have the Flu")
+        print("Your symptoms show that you might have the Flu.")
+
+else:
+    print("Your temperature is below normal, please visit the doctor immediately.")
