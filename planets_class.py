889564502561c1b3583a752868b31e3f16037df7
RealAce-Git-Hub <tyler.tuazon@gmail.com>
2025-08-07 20:56:10 +0700

    error in displaying planets_class.py matplotlib imshow()

--- planets_class.py
+++ planets_class.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 class Planets:
     def __init__(self, name, diameter, distance_from_sun, satellites=None, atmosphere=None, rotation_period=None, rotation_speed=None, revolution_period=None, revolution_speed=None, fun_fact=None):
@@ -13,25 +14,24 @@ class Planets:
         self.revolution_speed = revolution_speed
         self.fun_fact = fun_fact
 
-    def display(self):
-        self.image_location = f"planet_pics/{self.name.lower()}.jpeg"
-        print(self.image_location)
-        plt.imshow(plt.imread(self.image_location))
-        plt.axis("off")
-        plt.title(self.name)
-        if self.fun_fact:
-            plt.figtext(0.5, 0.01, self.fun_fact, wrap=True, horizontalalignment='center', fontsize=24)
-        plt.show()
-
 if __name__ == "__main__":
     jupiter = Planets("Jupiter", 143000.00, 777000000.00, ["Metis","Adestrea","Amalethea","Thebe","Io"], ["Hydrogen","Helium"], 9.9, 12.6, 11.9, 13.07, "Jupiter has a red spot with a size 3x of Earth.")
     venus = Planets("Venus", 12000.00, 108000000.00, None, ["Carbon Dioxide", "Sulfuric Acid"], 5832, 6.52, 244.7, 35.02, "A single day on Venus is longer than its year.")
     mercury = Planets("Mercury", 1550, 58000000, None, ["Oxygen","Sodium", "Hydrogen", "Helium", "Potassium"], 1392, 10.89, 88, 47.87, "Mercury has a crater named after Dr.Suess")
     saturn = Planets("Saturn", 120500.00, 1434000000, ["Titan","Enceladus","Hyperion","Promethius","Pandora"], ["Hydrogen", "Helium"], 10.7, 9.87, 2540, 9.69, "Saturn would float in water.")
     neptune = Planets("Neptune", 49528.00, 44708000000000.00, ["Triton","Despina","Galatea","Halimede","Hippocamp"], ["Hydrogen", "Helium"], 16.11, 2.68, 1436640, 5.43, "Neptune can rain diamonds")
-    
-    jupiter.display()
-    venus.display()
-    mercury.display()
-    saturn.display()
-    neptune.display()
+    mars = Planets("Mars", 6779.00, 241680000.00, ["Phobos","Deimos"], ["Carbon Dioxide","Nitrogen","Argon"], 24.62, 868, 24.6, 86870.78, "Mars has the tallest mountain in our solar system which is about 3x the height of Mount Everest")
+
+    image_datas = os.listdir("planet_pics")
+    image_datas_dir = []
+    for i in image_datas:
+        print("planet_pics/" + i)
+        image_datas_dir.append("planet_pics/" + i)
+
+    f, axarr = plt.subplots(2,3)
+    axarr[0,0].imshow(image_datas_dir[0])
+    axarr[0,1].imshow(image_datas_dir[1])
+    axarr[0,2].imshow(image_datas_dir[2])
+    axarr[1,0].imshow(image_datas_dir[3])
+    axarr[1,1].imshow(image_datas_dir[4])
+    axarr[1,2].imshow(image_datas_dir[5])
